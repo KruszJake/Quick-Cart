872c7208ede514a3eaec86df36479acac1aebb62
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-05-07 17:35:42 +0000

    Updating to latest FlutterFlow output.

--- lib/backend/backend.dart
+++ lib/backend/backend.dart
@@ -8,6 +8,7 @@ import 'schema/util/firestore_util.dart';
 import 'schema/users_record.dart';
 import 'schema/meals_record.dart';
 import 'schema/grocery_list_record.dart';
+import 'schema/items_record.dart';
 
 export 'dart:async' show StreamSubscription;
 export 'package:cloud_firestore/cloud_firestore.dart' hide Order;
@@ -19,6 +20,7 @@ export 'schema/util/schema_util.dart';
 export 'schema/users_record.dart';
 export 'schema/meals_record.dart';
 export 'schema/grocery_list_record.dart';
+export 'schema/items_record.dart';
 
 /// Functions to query UsersRecords (as a Stream and as a Future).
 Future<int> queryUsersRecordCount({
@@ -126,6 +128,43 @@ Future<List<GroceryListRecord>> queryGroceryListRecordOnce({
     queryCollectionOnce(
       GroceryListRecord.collection,
       GroceryListRecord.fromSnapshot,
+      queryBuilder: queryBuilder,
+      limit: limit,
+      singleRecord: singleRecord,
+    );
+
+/// Functions to query ItemsRecords (as a Stream and as a Future).
+Future<int> queryItemsRecordCount({
+  Query Function(Query)? queryBuilder,
+  int limit = -1,
+}) =>
+    queryCollectionCount(
+      ItemsRecord.collection,
+      queryBuilder: queryBuilder,
+      limit: limit,
+    );
+
+Stream<List<ItemsRecord>> queryItemsRecord({
+  Query Function(Query)? queryBuilder,
+  int limit = -1,
+  bool singleRecord = false,
+}) =>
+    queryCollection(
+      ItemsRecord.collection,
+      ItemsRecord.fromSnapshot,
+      queryBuilder: queryBuilder,
+      limit: limit,
+      singleRecord: singleRecord,
+    );
+
+Future<List<ItemsRecord>> queryItemsRecordOnce({
+  Query Function(Query)? queryBuilder,
+  int limit = -1,
+  bool singleRecord = false,
+}) =>
+    queryCollectionOnce(
+      ItemsRecord.collection,
+      ItemsRecord.fromSnapshot,
       queryBuilder: queryBuilder,
       limit: limit,
       singleRecord: singleRecord,
---
+++ lib/backend/schema/items_record.dart
@@ -0,0 +1,85 @@
+import 'dart:async';
+
+import 'package:collection/collection.dart';
+
+import '/backend/schema/util/firestore_util.dart';
+
+import 'index.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+
+class ItemsRecord extends FirestoreRecord {
+  ItemsRecord._(
+    DocumentReference reference,
+    Map<String, dynamic> data,
+  ) : super(reference, data) {
+    _initializeFields();
+  }
+
+  // "ItemName" field.
+  String? _itemName;
+  String get itemName => _itemName ?? '';
+  bool hasItemName() => _itemName != null;
+
+  void _initializeFields() {
+    _itemName = snapshotData['ItemName'] as String?;
+  }
+
+  static CollectionReference get collection =>
+      FirebaseFirestore.instance.collection('items');
+
+  static Stream<ItemsRecord> getDocument(DocumentReference ref) =>
+      ref.snapshots().map((s) => ItemsRecord.fromSnapshot(s));
+
+  static Future<ItemsRecord> getDocumentOnce(DocumentReference ref) =>
+      ref.get().then((s) => ItemsRecord.fromSnapshot(s));
+
+  static ItemsRecord fromSnapshot(DocumentSnapshot snapshot) => ItemsRecord._(
+        snapshot.reference,
+        mapFromFirestore(snapshot.data() as Map<String, dynamic>),
+      );
+
+  static ItemsRecord getDocumentFromData(
+    Map<String, dynamic> data,
+    DocumentReference reference,
+  ) =>
+      ItemsRecord._(reference, mapFromFirestore(data));
+
+  @override
+  String toString() =>
+      'ItemsRecord(reference: ${reference.path}, data: $snapshotData)';
+
+  @override
+  int get hashCode => reference.path.hashCode;
+
+  @override
+  bool operator ==(other) =>
+      other is ItemsRecord &&
+      reference.path.hashCode == other.reference.path.hashCode;
+}
+
+Map<String, dynamic> createItemsRecordData({
+  String? itemName,
+}) {
+  final firestoreData = mapToFirestore(
+    <String, dynamic>{
+      'ItemName': itemName,
+    }.withoutNulls,
+  );
+
+  return firestoreData;
+}
+
+class ItemsRecordDocumentEquality implements Equality<ItemsRecord> {
+  const ItemsRecordDocumentEquality();
+
+  @override
+  bool equals(ItemsRecord? e1, ItemsRecord? e2) {
+    return e1?.itemName == e2?.itemName;
+  }
+
+  @override
+  int hash(ItemsRecord? e) => const ListEquality().hash([e?.itemName]);
+
+  @override
+  bool isValidKey(Object? o) => o is ItemsRecord;
+}
